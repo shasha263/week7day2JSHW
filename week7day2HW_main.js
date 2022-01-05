869a7b17bab2f939fbfbdf60a2a3145fba83c933
shaharima Islam <shaharima@gmail.com>
2022-01-05 00:15:28 -0600

    First Commit

---
+++ week7day2HW_main.js
@@ -0,0 +1,77 @@
+//==========Exercise #1 ===========//
+/*
+Write a function that parses through the below object and displays all of their
+favorite food dishes as shown:
+*/
+
+let person3 = {
+    pizza:["Deep Dish","South Side Thin Crust"],
+    tacos:"Anything not from Taco bell",
+    burgers:"Portillos Burgers",
+    ice_cream:["Chocolate","Vanilla","Oreo"],
+    shakes:[{
+        oberwise:"Chocolate",
+        dunkin:"Vanilla",
+        culvers:"All of them",
+        mcDonalds:"Sham-rock-shake",
+        cupids_candies:"Chocolate Malt"
+    }]
+}
+console.log("Option 1")
+for(k in person3){
+    console.log(` ${k}: ${person3[k]}`); //must use bracket notation to access the values in this kind of loop
+}
+
+console.log("Option 2")
+
+//console.log(Object.keys(person3));
+
+console.log(Object.values(person3));
+//=======Exercise #2=========//
+/*
+Write an object prototype for a Person that has a name and age, has a printInfo method, and also has a method that 
+increments the persons age by 1 each time the method is called. Create two people using the 'new' keyword, and print 
+both of their infos and increment one persons age by 3 years. Use an arrow function for both methods
+*/
+
+// Create our Person Prototype
+// Use an arrow to create the printInfo method
+// Create another arrow function for the addAge method that takes a single parameter
+// Adding to the age 
+
+function Person(name, age){
+    this.name = name;
+    this.age = age;   
+
+    // JS prototype object can have methods
+
+    this.addAge = () => {
+        this.age1=age+3
+        console.log(` ${this.name} is ${this.age} plus 3.`);
+    }
+    this.printInfo = () => {
+        console.log(` ${this.name} is ${this.age} years old.`);
+    }
+
+   this.newAge =() => {
+        this.newAge=this.age +3
+        console.log(`${this.name} is ${this.newAge} years old`)
+
+   }   
+}
+
+let george_clooney = new Person('George Clooney', '60');
+let tom_cruise = new Person('Tom Cruise', '59');    
+
+george_clooney.printInfo();
+tom_cruise.printInfo();
+george_clooney.newAge();
+tom_cruise.newAge();
+
+// =============Exercise #3 ============//
+/*
+
+    Create a Promised based function that will check a string to determine if it's length is greater than 10.
+    If the length is greater than ten console log "Big word". 
+    If the length of the string is less than 10 console log "Small Number"
+*/
